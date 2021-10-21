3a4d6c9a22db6940d82974e7478d76112efda71c
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-10-21 11:43:34 +0300

    Add initial recipe book cover view
    Fixes #1

--- client/src/components/RecipeBookCover.js
+++ client/src/components/RecipeBookCover.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import '../styles/RecipeBookCover.scss'
 
-const RecipeBookCover = () => {
+const RecipeBookCover = ({ recipes }) => {
   return (
     <>
-      <p>RecipeBookCover</p>
+      <div class="recipe-book-cover">
+        <h2>{ recipes.books[0].name }</h2>
+      </div>
+      <h3>Näytä reseptit käyttäjän mukaan</h3>
+      <ul>
+        <li>Kaisa Rautiainen</li>
+        <li>Helena Rautiainen</li>
+        <li>Anton Rautiainen</li>
+      </ul>
+      <button onClick={ () => alert('not yet implemented') }>
+        Kopioi rekisteröitymislinkki
+      </button>
+      <p>Salaisen rekisteröitymilinkin kautta liittymällä sukulaisesi saavat oikeuden tarkastella { recipes.books[0].name }a ja lisätä omia reseptejään.</p>
     </>
   )
 }
