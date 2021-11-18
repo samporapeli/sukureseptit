48a3d981e36f509c376cc6ea47a876ddfc69da1d
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-11-18 18:36:53 +0200

    Change IDs from counting integer to UUID

--- server/models/Ingredient.js
+++ server/models/Ingredient.js
@@ -3,6 +3,12 @@ const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Ingredient extends Model {}
   Ingredient.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
     amount: {
       type: DataTypes.INTEGER,
       allowNull: true,
--- server/models/RecipeComment.js
+++ server/models/RecipeComment.js
@@ -3,6 +3,12 @@ const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class RecipeComment extends Model {}
   RecipeComment.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
     comment: {
       type: DataTypes.STRING,
       allowNull: false,
