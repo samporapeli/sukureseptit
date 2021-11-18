abdbf9ae2aa8e93c4900be6ebb821037b78b03fe
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-11-18 11:52:50 +0200

    Add router to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import LoggedInView from './components/LoggedInView'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <div>
-        <LoggedInView />
-      </div>
+      <Routes>
+        <Route path='logged-in' element={<LoggedInView />} />
+        <Route path='/' element={
+          <h2>Please <Link to={'logged-in'}>Log in</Link></h2>
+        } />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   )
 }
 
