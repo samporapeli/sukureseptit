7b98f0a692884e9d54094be47ad07c0d6c0beee1
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-11-23 19:15:43 +0200

    Use right recipe book instead of the first

--- client/src/components/RecipeBookCover.js
+++ client/src/components/RecipeBookCover.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import '../styles/RecipeBookCover.scss'
 
 const RecipeBookCover = ({ recipes, family }) => {
+  const params = useParams()
   return (
     <>
       <div className="recipe-book-cover">
-        <h2>{ recipes ? recipes.books[0].name : 'Ladataan...' }</h2>
+        <h2>{ recipes
+          ? recipes.books.find(b => b.id = params.bookID).name
+          : 'Ladataan...'
+        }</h2>
       </div>
       <h3>Näytä reseptit käyttäjän mukaan</h3>
       <ul className='list-disc px-6'>
@@ -20,7 +25,7 @@ const RecipeBookCover = ({ recipes, family }) => {
       <button className='btn btn-green' onClick={ () => alert('not yet implemented') }>
         Kopioi rekisteröitymislinkki
       </button>
-      <p>Salaisen rekisteröitymilinkin kautta liittymällä sukulaisesi saavat oikeuden tarkastella { recipes ? recipes.books[0].name : 'reseptikirja'}a ja lisätä omia reseptejään.</p>
+      <p>Salaisen rekisteröitymilinkin kautta liittymällä sukulaisesi saavat oikeuden tarkastella { recipes ? recipes.books.find(b => b.id = params.bookID).name : 'reseptikirja'}a ja lisätä omia reseptejään.</p>
     </>
   )
 }
