6e6c04a4772004e5be128bb98214a42678f79ca2
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-12-02 11:25:27 +0200

    Translate to Finnish and add headings

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -6,12 +6,16 @@ const Recipe = ({ recipe }) => {
       <div className="RecipePage">
         <p>{recipe.user} · {recipe.mealType}</p>
         <h2>{recipe.name}</h2>
-        <p>{recipe.portions} portions</p>
-        <p>Cooking time: {recipe.cookingTime}</p>
+        <p>{recipe.portions} annosta</p>
+        <p>Valmistusaika: {recipe.cookingTime}</p>
+        <h3>Ainesosat</h3>
         <ul>{recipe.Ingredients.map(({amount, name, unit}) => (
           <li key={name}>{amount} {unit} {name}</li>
         ))}</ul>
+        <h3>Valmistusohjeet</h3>
         <p>{recipe.instructions}</p>
+        <h3>Kommentit</h3>
+        <p>TODO: comments</p>
       </div>
     </>
   )
